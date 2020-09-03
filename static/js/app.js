24e08806e987e6b3368754911447ebefdb20c20b
Miguel S. Flores <msflores10307@gmail.com>
2020-09-03 00:29:57 -0400

    adding comments to javascript

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,55 @@
-
+// set filepath for data
 var filepath = "../../samples.json";
 
+// import data and trigger main update function
 d3.json(filepath).then(updateDashboard);
 
-
+// this function is the primary function for populating menu options and refreshing metadata panels 
 function updateDashboard(data) {
 
+    // this section of code pulls names from data and dynamically appends options to menu 
     var names = data.names;
-    var menu = d3.select('#selDataset')
-
+    var menu = d3.select('#selDataset');
     names.forEach((name) => {
     var option = menu.append("option");
     option.text(name)
 
+    // this section of code pulls samples and metadata from raw data
     var samples = data.samples;
     var meta = data.metadata;
 
+    // this section of code is an event listener that will trigger an refresh of the 
+    // visualizations on the page when a change is detected in the menu selection
     menu.on('change', function (){ 
-        var sampleChoice = menu.property("value")
-        console.log(sampleChoice)
+        // selects menu selection value
+        var sampleChoice = menu.property("value");
+        console.log(sampleChoice);
 
+        // removes content from metadata panel
         var selection =  d3.select("#sample-metadata").selectAll(".demodata");
         selection.data([]).exit().remove();
-        newViz(sampleChoice,samples,meta)
+
+        // calls function to populate dashboard with new visualizations
+        newViz(sampleChoice,samples,meta);
     })
   });
 };
 
-
+// this function is called to update all the visualizations for a new menu item selection.
 function newViz(id,samples,meta){
+    // this loops through each item to find the id that matches the menu selection. 
     samples.forEach((subject) => {
         if (subject.id === id) {
+            // if a match is found, this section of code sorts and extracts the sample data into usable arrays
             var subject = [subject]
             var sortedSubject = subject.sort((a,b) => b.sample_values-a.sample_values);
             var barData = Object.values(sortedSubject)[0].sample_values;
-            
             var barLabelsRaw = Object.values(sortedSubject)[0].otu_ids;
-            var barLabels = barLabelsRaw.map(x=>`OTU-${x.toString()} `)
+            var barLabels = barLabelsRaw.map(x=>`OTU-${x.toString()} `);
             var barHover = Object.values(sortedSubject)[0].otu_labels;
             
-
-
-        var barTrace = [{
+            // this uses arrays above to define trace for bar chart. Note only top 10 values are taken
+            var barTrace = [{
             type: 'bar',
             x: barData.slice(0,10),
             y: barLabels.slice(0,10),
@@ -51,9 +59,11 @@ function newViz(id,samples,meta){
             yaxis: {title:'OTU ID'},
             xaxis: {title: 'Sample Value'}
         }];
-                
+        
+        // this line of code creates horizontal bar chart
         Plotly.newPlot('bar', barTrace);
 
+        // this section of code creates the trace for the bubble chart
         var bubbleTrace = {
             x: barLabelsRaw,
             y: barData,
@@ -65,7 +75,7 @@ function newViz(id,samples,meta){
             }
           };
           
-          
+          // this section of code creates the layout attribute for the bubble chart
           var bubbleLayout = {
             title: 'OTU Distribution',
             showlegend: false,
@@ -73,17 +83,21 @@ function newViz(id,samples,meta){
             width: 1000
           };
           
+          // creates bubble chart
           Plotly.newPlot('bubble', [bubbleTrace], bubbleLayout);
             }
         }) 
 
-    
+        // this is a loop to pull metadata for subject with an id matching menu selection
         meta.forEach((person) => {
         if (person.id == id) {
+
+            // if a match is found, this section of code extracts metadata into usable array
             var metaPoints = Object.entries(person);
-            var keyValues = metaPoints.map(x=>`${x[0]}: ${x[1]}`)
+            var keyValues = metaPoints.map(x=>`${x[0]}: ${x[1]}`);
             console.log(keyValues)
 
+            // this section of code appends div elements to the metadata panel on the dashboard
             var selection2 =  d3.select("#sample-metadata").selectAll(".demodata");
             selection2.data(keyValues).enter().append("div").classed("demodata",true)
             .html(function(d) {
@@ -91,13 +105,13 @@ function newViz(id,samples,meta){
             })
             ;
 
+            // this section of data defines the trace for the guage diagram
             var gaugeData = [
                 {
                   type: "indicator",
                   mode: "gauge+number+delta",
                   value: metaPoints[6][1],
                   title: { text: "Washing Frequency", font: { size: 24 } },
-                //   delta: { reference: 7, increasing: { color: "RebeccaPurple" } },
                   gauge: {
                     axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue" },
                     bar: { color: "darkblue" },
@@ -117,6 +131,7 @@ function newViz(id,samples,meta){
                 }
               ];
               
+              // this section of code defines the layout for the gauge diagram
               var layout = {
                 width: 500,
                 height: 400,
@@ -125,6 +140,7 @@ function newViz(id,samples,meta){
                 font: { color: "darkblue", family: "Arial" }
               };
               
+              // creates guage diagram
               Plotly.newPlot('gauge', gaugeData, layout);    
             
         }
